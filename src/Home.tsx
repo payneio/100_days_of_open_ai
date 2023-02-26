35a6d675a878d57660a8fe47a401e6008833e01d
Paul Payne <paul@payne.io>
2023-02-26 12:24:51 -0800

    Day 00 description.

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Layout = () => {
     return (
       <main style={{ padding: '1rem 0' }}>
-        <Link to="/day-00">Day 00</Link>
+        <Link to="/day-00">Day 00: Setup</Link>
       </main>
     );
   };
