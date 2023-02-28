aaa4496ad43a04973b315c72b316b8861ceef2f1
Paul Payne <paul@payne.io>
2023-02-28 09:34:06 -0800

    Extract prompter.

--- src/Experiments/Day00.tsx
+++ src/Experiments/Day00.tsx
@@ -1,41 +1,7 @@
-import { useState } from 'react'
-import {
-  Label,
-  Link,
-  PrimaryButton,
-  Stack,
-  Text,
-  TextField,
-} from '@fluentui/react'
-import useOpenAI from '../lib/hooks/useOpenAI'
-import { hasOpenAIKey } from '../lib/state'
+import { Link, Stack, Text } from '@fluentui/react'
+import PromptRunner from '../components/PromptRunner'
 
 function Day00() {
-  const [prompt, setPrompt] = useState('Say this is a test')
-  const [response, setResponse] = useState('No response yet')
-  const openai = useOpenAI()
-  const [runDisabled, setRunDisabled] = useState(!hasOpenAIKey())
-
-  const handlePrompt = (
-    _: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
-    newValue?: string
-  ) => {
-    setPrompt(newValue || '')
-  }
-
-  const handleClick = async () => {
-    if (!openai) return
-    setRunDisabled(true)
-    const response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: prompt,
-      temperature: 0,
-      max_tokens: 1024,
-    })
-    setRunDisabled(!hasOpenAIKey())
-    setResponse(response.data.choices[0].text || 'error')
-  }
-
   return (
     <Stack tokens={{ childrenGap: 10 }}>
       <Text variant="large">Day 00</Text>
@@ -63,25 +29,7 @@ function Day00() {
         </Link>{' '}
         and click "Create API Key".
       </Text>
-      <TextField
-        label="Prompt"
-        multiline
-        rows={6}
-        value={prompt}
-        onChange={handlePrompt}
-      />
-      <PrimaryButton text="Run" onClick={handleClick} disabled={runDisabled} />
-      <Stack
-        style={{
-          minHeight: '150px',
-          backgroundColor: 'lightgray',
-          padding: '1rem',
-        }}
-        tokens={{ childrenGap: 10 }}
-      >
-        <Label>GPT response:</Label>
-        <Text variant="large">{response}</Text>
-      </Stack>
+      <PromptRunner />
     </Stack>
   )
 }
---
+++ src/components/PromptRunner.tsx
@@ -0,0 +1,118 @@
+import {
+  CommandBar,
+  Label,
+  Text,
+  Stack,
+  TextField,
+  ICommandBarItemProps,
+  PrimaryButton,
+  StackItem,
+  ITextField,
+} from '@fluentui/react'
+import useOpenAI from '../lib/hooks/useOpenAI'
+import { hasOpenAIKey } from '../lib/state'
+import { useState, useRef } from 'react'
+
+const PromptRunner = () => {
+  const [response, setResponse] = useState('No response yet')
+  const [prompt, setPrompt] = useState('Say this is a test')
+  const [loading, setLoading] = useState(false)
+  const inputRef = useRef<ITextField>(null)
+
+  const runDisabled = !hasOpenAIKey()
+  const hasMenu = false
+  const hasOutput = false
+
+  const openai = useOpenAI()
+
+  const handlePrompt = (
+    _: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>,
+    newValue?: string
+  ) => {
+    setPrompt(newValue || '')
+  }
+
+  const handleClick = async () => {
+    if (!openai || !inputRef?.current?.value) return
+    setLoading(true)
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: inputRef.current.value,
+      temperature: 0,
+      max_tokens: 1024,
+    })
+    setLoading(false)
+    setPrompt(prompt + response.data.choices[0].text)
+    setResponse(response.data.choices[0].text || 'error')
+  }
+
+  const _items: ICommandBarItemProps[] = []
+
+  const _farItems: ICommandBarItemProps[] = [
+    {
+      key: 'play',
+      text: 'Play',
+      ariaLabel: 'play',
+      iconOnly: true,
+      iconProps: {
+        iconName: loading ? 'ProgressRingDots' : 'Play',
+      },
+      disabled: runDisabled,
+      onClick: () => handleClick() as any,
+    },
+  ]
+
+  // light blue background with transparent buttons
+  const commandBarStyles = {
+    root: {
+      backgroundColor: 'lightblue',
+      // transparent buttons
+      selectors: {
+        '.ms-Button': {
+          backgroundColor: 'transparent',
+        },
+      },
+    },
+  }
+
+  return (
+    <Stack tokens={{ childrenGap: '10px' }}>
+      <StackItem>
+        {hasMenu && (
+          <CommandBar
+            items={_items}
+            farItems={_farItems}
+            styles={commandBarStyles}
+          />
+        )}
+        <TextField
+          multiline
+          rows={15}
+          value={prompt}
+          onChange={handlePrompt}
+          componentRef={inputRef}
+        />
+      </StackItem>
+      <Stack horizontal tokens={{ childrenGap: '10px' }}>
+        <PrimaryButton onClick={handleClick} disabled={runDisabled || loading}>
+          Run
+        </PrimaryButton>
+      </Stack>
+      {hasOutput && (
+        <Stack
+          style={{
+            minHeight: '150px',
+            backgroundColor: 'lightgray',
+            padding: '1rem',
+          }}
+          tokens={{ childrenGap: 10 }}
+        >
+          <Label>Output:</Label>
+          <Text variant="large">{response}</Text>
+        </Stack>
+      )}
+    </Stack>
+  )
+}
+
+export default PromptRunner
